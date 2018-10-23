9e74443d1150228203260931a88277974abb1432
markinsane <markguanchah@gmail.com>
2018-10-23 11:44:06 -0600

    commit

--- src/pages/leaderboard/leaderboard.ts
+++ src/pages/leaderboard/leaderboard.ts
@@ -60,7 +60,7 @@ export class LeaderboardPage {
           }
         })
 
-        this.storage.set('leaderboard', JSON.stringify(res));
+        this.storage.set('leaderboard', "");
          console.log("result")
       })
     })
